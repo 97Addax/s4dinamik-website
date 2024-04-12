500ee8e0143e688adb089ac54e5143fde4b0ae50
asfa <iamasfa97@gmail.com>
2024-04-12 15:43:08 +0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/s4dinamik-website/',
+  base: "/s4dinamik-website/",
   plugins: [
     vue(),
   ],
